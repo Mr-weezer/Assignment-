63c028daecf08f98e4acc2cffb575154bc563f8d
ItsWhiza <mrweezer286@gmail.com>
2024-10-18 00:40:01 -0700

    Add files via upload
    Arrays

---
+++ 1D.c
@@ -0,0 +1,15 @@
+//Programms for 1D  arrays
+/*author:Francis Mwiga
+Reg No:CT101/G/22131/24
+*/
+#include <stdio.h>
+
+int main()
+{  
+  int i;
+  int marks[6]={10,20,30,40,50,60};
+    for(i=0;i<6;i++){
+        printf("marks[%d]= %d\n",i,marks[i]);
+        }
+return 0;
+}
